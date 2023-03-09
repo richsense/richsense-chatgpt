df05f3ada56c71dc8ee9f4ef8e109a2dbd500a19
richsense <iamrich.cc@gmail.com>
2023-03-10 01:48:55 +0800

    init richsense-chatgpt repository

---
+++ src/default.ts
@@ -0,0 +1,11 @@
+export const defaultSetting = {
+  continuousDialogue: true,
+  archiveSession: false,
+  openaiAPIKey: "",
+  openaiAPITemperature: 60,
+  systemRule: ""
+}
+
+export const defaultMessage = `
+- 本站仅用于演示，填入自己的 key 才可使用。
+- 由 [OpenAI API (gpt-3.5-turbo)](https://platform.openai.com/docs/guides/chat) 和 [Vercel](http://vercel.com/) 提供支持。`
